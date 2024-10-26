dbd471f45a4ffbb8536f3a29c7aa7cbb3ae5e3a7
Sapdotten <semenova.a.p.04@gmail.com>
2024-10-26 16:54:55 +0400

    change script for parsing groups from site

--- get_groups.py
+++ get_groups.py
@@ -3,34 +3,26 @@ import requests
 import json
 
 
-def get_links():
+if __name__ == "__main__":
     data = {}
-    html = requests.get("https://ssau.ru/rasp").text
-    page = BeautifulSoup(html, 'lxml')
-    facultets = page.find_all('div', class_='card-default faculties__item')
-    for elem in facultets:
-        links = elem.find_all('a', href=True)
-        for link in links:
-            data[elem.text.strip()] = link['href']
-    return data
+    html = requests.get("https://ssau.ru/education/program").text
+    page = BeautifulSoup(html, "lxml")
+    h_and_tables = page.find("div", class_="body1-text")
+    hh = []
+    groups = []
+    for elem in h_and_tables:
+        if elem.name == "h4":
+            hh.append(elem.text.strip())
+            groups.append([])
+        elif elem.name == "table":
+            trs = elem.find_all("tr")
+            for tr in trs[2:]:
+                tds = tr.find_all("td")
+                groups[-1].append(tds[1].text.replace(".", "").strip())
 
+    data = {}
+    for i in range(0, len(hh)):
+        data[hh[i]] = groups[i]
 
-def get_groups_via_links(data: dict) -> dict[str, set[str]]:
-    group_fac = {}
-    for key in data.keys():
-        html = requests.get("https://ssau.ru" + data[key]).text
-        page = BeautifulSoup(html, 'lxml')
-        groups = page.find_all('a', class_="btn-text group-catalog__group")
-        group_fac[key] = set()
-        for group in groups:
-            group_fac[key].add(group.text[6:].strip())
-        for key in data.keys():
-            data[key] = list(data[key])
-    return group_fac
-
-
-if __name__ == '__main__':
-    data = get_links()
-    data = get_groups_via_links(data)
-    with open("groups.json", 'w', encoding='UTF-8') as file:
+    with open("parsed_groups.json", "w", encoding="UTF-8") as file:
         json.dump(data, file, ensure_ascii=False)
